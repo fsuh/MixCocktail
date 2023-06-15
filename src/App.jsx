bcc256bbe26685daf29f1089f723089ab2a659f2
Fuh Suh <tofuhsuh@gmail.com>
2023-06-15 10:32:56 +0300

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+	About,
+	HomeLayout,
+	Landing,
+	Error,
+	NewsLetter,
+	Cocktail,
+} from "./pages";
+
+import { loader as landingLoader } from "./pages/Landing";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+				loader: landingLoader,
+			},
+			{
+				path: "cocktail",
+				element: <Cocktail />,
+			},
+			{
+				path: "newsletter",
+				element: <NewsLetter />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+		],
+	},
+]);
+const App = () => {
+	return <RouterProvider router={router} />;
+};
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Landing from "./Landing";
+import Cocktail from "./Cocktail";
+import NewsLetter from "./NewsLetter";
+import HomeLayout from "./HomeLayout";
+import Error from "./Error";
+
+export { About, Landing, Cocktail, NewsLetter, HomeLayout, Error };
